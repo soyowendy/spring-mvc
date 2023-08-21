52ee142c20e45c43f80e85c7ae7539c8260cef8e
Wenderson Santos <souowendy@gmail.com>
2023-08-21 13:58:13 -0300

    Create first page

---
+++ src/main/java/br/com/souowendy/springmvc/controller/HomeController.java
@@ -0,0 +1,15 @@
+package br.com.souowendy.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+	@GetMapping
+	public String home() {
+		return "home";
+	}
+}
