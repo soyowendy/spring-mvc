73118d5745e9a7fd7df5c2b182a10791fa5a0a93
Wenderson Santos <souowendy@gmail.com>
2023-08-21 13:04:00 -0300

    Initial Commit

---
+++ src/main/java/br/com/souowendy/springmvc/SpringMvcProjectApplication.java
@@ -0,0 +1,13 @@
+package br.com.souowendy.springmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcProjectApplication.class, args);
+	}
+
+}
