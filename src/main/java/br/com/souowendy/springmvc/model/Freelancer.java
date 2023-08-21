4138aa91ee16ab136f554a30255a8af63802723a
Wenderson Santos <souowendy@gmail.com>
2023-08-21 15:15:09 -0300

    Create Freelancer model and connect mysql db

---
+++ src/main/java/br/com/souowendy/springmvc/model/Freelancer.java
@@ -0,0 +1,76 @@
+package br.com.souowendy.springmvc.model;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+public class Freelancer {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(nullable = false, length = 100)
+	private String nome;
+	@Column(nullable = false, length = 15)
+	private String telefone;
+	@Column(nullable = false)
+	private String endereco;
+
+	public Freelancer() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Freelancer that = (Freelancer) o;
+		return Objects.equals(id, that.id);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public String toString() {
+		return "Freelancer{" +
+				"id=" + id +
+				", nome='" + nome + '\'' +
+				", telefone='" + telefone + '\'' +
+				", endereco='" + endereco + '\'' +
+				'}';
+	}
+}
