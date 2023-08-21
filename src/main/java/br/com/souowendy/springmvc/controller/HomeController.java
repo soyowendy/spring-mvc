1a2992639cf3d1ee9c422d5631896b14c6704698
Wenderson Santos <souowendy@gmail.com>
2023-08-21 14:21:31 -0300

    Send information from Controller to View

--- src/main/java/br/com/souowendy/springmvc/controller/HomeController.java
+++ src/main/java/br/com/souowendy/springmvc/controller/HomeController.java
@@ -1,15 +1,25 @@
 package br.com.souowendy.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
-	@GetMapping
-	public String home() {
+	@GetMapping("/hellonome")
+	public String helloNome(ModelMap modelMap) {
+		modelMap.addAttribute("nome", "Wenderson");
 		return "home";
+	}
+
+	@GetMapping("/helloapelido")
+	public ModelAndView helloApelido() {
+		ModelAndView modelAndView = new ModelAndView("home");
+		modelAndView.addObject("nome", "Wendy");
+		return modelAndView;
 	}
 }
